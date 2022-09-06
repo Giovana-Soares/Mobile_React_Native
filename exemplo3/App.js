c38bd565546883d86daae2cda4a9014c79f533fc
Giovana <giihs2005@gmail.com>
2022-09-05 21:57:53 -0300

    Introdução a botões

---
+++ exemplo3/App.js
@@ -0,0 +1,13 @@
+import * as react from 'react';
+import {View} from 'react-native';
+import contador from ' ./componentes/contador.js';
+
+export default function App(){
+  return (
+    <View>
+        <contador>
+          
+        </contador>
+    </View>
+  )
+}
